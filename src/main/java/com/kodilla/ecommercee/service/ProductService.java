a16c4182c8dfd058797e1576d8945a815e431381
Tomasz Siwiec <tomasz.siwiec@motorolasolutions.com>
2019-11-26 10:38:01 +0100

    [JDP191101-1] Deleted unneeded files

--- src/main/java/com/kodilla/ecommercee/service/ProductService.java
+++ src/main/java/com/kodilla/ecommercee/service/ProductService.java
@@ -3,45 +3,30 @@ package com.kodilla.ecommercee.service;
 import com.kodilla.ecommercee.domain.Product;
 import com.kodilla.ecommercee.domain.ProductDto;
 import com.kodilla.ecommercee.domain.ProductNotFoundException;
-import com.kodilla.ecommercee.mapper.ProductMapper;
-import com.kodilla.ecommercee.repository.ProductRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class ProductService {
 
-    @Autowired
-    private ProductRepository productRepository;
-
-    @Autowired
-    private ProductMapper productMapper;
-
     public List<ProductDto> getProducts() {
-        return productMapper.mapToProductDtoList(productRepository.findAll());
+        return new ArrayList<>();
     }
 
     public ProductDto getProductById(Long id) throws ProductNotFoundException {
-        Product product = productRepository.findById(id).orElseThrow(ProductNotFoundException::new);
-        return productMapper.mapToProductDto(product);
+        return new ProductDto();
     }
 
     public Product saveProduct(ProductDto productDto) {
-        return productRepository.save(productMapper.mapToProduct(productDto));
+        return new Product();
     }
 
     public ProductDto updateProduct(ProductDto productDto) throws ProductNotFoundException {
-        Product product = productRepository.findById(productDto.getId()).orElseThrow(ProductNotFoundException::new);
-        product.setName(productDto.getName());
-        product.setDescription(productDto.getDescription());
-        product.setPrice(productDto.getPrice());
-        productRepository.save(product);
-        return productMapper.mapToProductDto(product);
+        return new ProductDto();
     }
 
     public void deleteProductById(Long id) {
-        productRepository.deleteById(id);
     }
 }
